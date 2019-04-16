e11c38d1c9a0ee6ba4709a95dda15207de045fb4
Musacoli <collins.mbulakyalo@andela.com>
2019-04-16 15:49:01 +0300

    Setup the empty react project:
    [Finishes #2 #3 #5]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import createAFLStore from './store/index';
+import routes from './routes/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+	<Provider store={createAFLStore()}>
+		<Router>
+			<Switch>
+				{routes.map((route) => {
+					return (
+						<Route
+							path={route.path}
+							component={route.component}
+							key={route.path}
+						/>
+					);
+				})}
+			</Switch>
+		</Router>
+	</Provider>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(app, document.getElementById('root'));
---
+++ src/tests/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import Home from '../containers/Home';
+import createAFLStore from '../store/index';
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(
+		<Provider store={createAFLStore()}>
+			<Home />
+		</Provider>,
+		div,
+	);
+	ReactDOM.unmountComponentAtNode(div);
+});
