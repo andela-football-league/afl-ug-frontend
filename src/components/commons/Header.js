9a1856a180facc4e0dbf408f4a8fe9cd540c1d61
Musacoli <collins.mbulakyalo@andela.com>
2019-05-01 03:29:24 +0300

    feat(Footer): Creates a footer component that is reusable.
    [Closes #8]

--- src/components/commons/Header.js
+++ src/components/commons/Header.js
@@ -1,58 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import '../../assets/styles/Defaults.scss';
+import '../../assets/styles/commons/Header.scss';
 import AFL from '../../assets/images/AFL.svg';
 
-const Header = ({ loggedIn, pageTitle }) => (
-	<div className="header-container">
-		<div className="header-img">
-			<div className="header-section">
-				<div className="top-div">
-					{loggedIn ? (
-						<h1 className="log">PROFILE</h1>
-					) : (
-						<h1 className="log">LOGIN</h1>
-					)}
+const Header = (props) => {
+	const { loggedIn, pageTitle } = props;
+	return (
+		<header className="header-container">
+			<div className="header-img">
+				<div className="header-section">
+					<div className="top-div">
+						<button type="button" className="log">
+							{loggedIn ? 'PROFILE' : 'LOG IN'}
+						</button>
+					</div>
+					<div>
+						<div className="bottom-headings">
+							<button type="button">STANDINGS</button>
+							<button type="button">TEAMS</button>
+							<button type="button">STATISTICS</button>
+						</div>
+					</div>
 				</div>
-				<div>
-					<div className="bottom-headings">
-						<h1>STANDINGS</h1>
-						<h1>TEAMS</h1>
-						<h1>STATISTICS</h1>
+				<div className="header-logo">
+					<img
+						src={
+							AFL ||
+							'https://s3.us-east-2.amazonaws.com/afl1/AFL-02.svg'
+						}
+						alt="AFL logo"
+					/>
+				</div>
+				<div className="header-section">
+					<div className="top-div">
+						<button type="button" className="reg">
+							{loggedIn ? 'LOG OUT' : 'REGISTER'}
+						</button>
+					</div>
+					<div>
+						<div className="bottom-headings">
+							<button type="button">FIXTURE</button>
+							<button type="button">RESULTS</button>
+							<button type="button">ASSOCIATION</button>
+						</div>
 					</div>
 				</div>
 			</div>
-			<div className="header-logo">
-				<img
-					src={
-						AFL ||
-						'https://s3.us-east-2.amazonaws.com/afl1/AFL-02.svg'
-					}
-					alt="AFL logo"
-				/>
+			<div>
+				<h1 className="heading">{pageTitle}</h1>
 			</div>
-			<div className="header-section">
-				<div className="top-div">
-					{loggedIn ? (
-						<h1 className="reg">LOG OUT</h1>
-					) : (
-						<h1 className="reg">REGISTER</h1>
-					)}
-				</div>
-				<div>
-					<div className="bottom-headings">
-						<h1>FIXTURE</h1>
-						<h1>RESULTS</h1>
-						<h1>ASSOCIATION</h1>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div>
-			<h1 className="heading">{pageTitle}</h1>
-		</div>
-	</div>
-);
+		</header>
+	);
+};
 
 Header.defaultProps = {
 	pageTitle: '',
